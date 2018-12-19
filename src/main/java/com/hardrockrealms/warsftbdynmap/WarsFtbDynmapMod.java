3fc2c51bd77203f23472b61493587b57caa310ae
Warmad0311 <warmad0311@gmail.com>
2018-12-19 13:13:42 -0500

    Add logo and fix version number

--- src/main/java/com/hardrockrealms/warsftbdynmap/WarsFtbDynmapMod.java
+++ src/main/java/com/hardrockrealms/warsftbdynmap/WarsFtbDynmapMod.java
@@ -30,7 +30,7 @@ public class WarsFtbDynmapMod
 {
     public static final String MODID = "warsftbdynmap";
     public static final String NAME = "WarsFtbDynmap";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.0.2";
 
     private static final String DYNMAP_MODID = "dynmap";
     private static final String FTBUTILITIES_MODID = "ftbutilities";
